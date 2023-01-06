a6e76e777669af355db171533c90fa54f931f77e
Anderson Lima <anderson_developer@outlook.com>
2023-01-06 14:28:32 -0300

    🔥 feat: adding models | post listing at API.

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const {ApolloServer} = require ('apollo-server');
-const gql  = require ('graphql-tag');
-const mongoose = require ('mongoose');
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
-const {MONGODB} = require('./config.js');
+const Post = require("./models/Post");
+const { MONGODB } = require("./config.js");
 
-const typeDefs = gql `
-    type Query{
-      sayHi: String!
-    }
-`
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World!'
-  }
-}
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(MONGODB, {useNewUrlParser: true})
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to MongoDB!');
-    return server.listen({port: 5000})
+    console.log("Connected to MongoDB!");
+    return server.listen({ port: 5000 });
   })
   .then((res) => {
-    console.log(`Server running at ${res.url}`)
-  })
+    console.log(`Server running at ${res.url}`);
+  });
